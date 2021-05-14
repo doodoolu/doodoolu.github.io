e488e912558ab611a0ae4df3f93eef57da00ada3
Vincent <tnecniv43208@gmail.com>
2021-05-14 11:48:52 +0800

    update again

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var user = localStorage['User'];
-var user = 'b06703012'
+var user = 'b06302352'
 console.log(localStorage['User']);
 const HW_NO = 5
 
@@ -247,7 +247,6 @@ function createMultiChart(ctx, labels, data) {
 var multiChart = document.getElementById('multiChart');
 time_db.database().ref().on('value', snapshot => {
     let snap = snapshot.val()
-    console.log(snap)
     let ac_rate = Object.values(snap['AC Rate'])
     ac_rate = ac_rate.map(function(e) {
         return e.toFixed(4)
@@ -260,3 +259,85 @@ time_db.database().ref().on('value', snapshot => {
     })
     createMultiChart(multiChart, labels, [ac_rate, ac, wa])
 })
+
+function getPercentData() {
+    return new Promise((resolve, reject) => {
+        rankings_db.database().ref().on('value', snapshot => {
+            let snap = snapshot.val();
+            resolve(snap[user]);
+        })
+    })
+}
+async function initializeProgressBar() {
+    let percentage_chart = document.getElementById('hw_percentage')
+    let progress_bar = percentage_chart.querySelectorAll('[role="progressbar"]');
+    let progress_percentage = percentage_chart.getElementsByTagName('span');
+    let hw_name = percentage_chart.getElementsByTagName('h4');
+
+    let progress_dropdown = document.getElementById('progress_dropdown').getElementsByTagName('button');
+    let dropdown_button_name = document.getElementById('percent_button');
+    let user_percent = await getPercentData();
+    let bar_6_exist = true;
+
+    for (let i = 0; i < 5; i++) {
+        progress_bar[i].style.width = user_percent['HW' + (i + 1).toString() + ' Total Score'].toString() + '%';
+        progress_percentage[i].textContent = user_percent['HW' + (i + 1).toString() + ' Total Score'].toString() + '%'
+        hw_name[i].childNodes[0].textContent = 'HW' + (i + 1).toString();
+
+    }
+    for (let i = 0; i < progress_dropdown.length; i++) {
+        if (i == 0) {
+            progress_dropdown[i].onclick = function() {
+                if (!bar_6_exist) {
+                    let h4 = document.createElement('h4');
+                    h4.classList.add('small', 'font-weight-bold');
+                    h4.textContent = 'HW6';
+                    let span = document.createElement('span');
+                    span.classList.add('float-right');
+                    span.textContent = 'XXX%'
+                    h4.appendChild(span);
+                    let div = document.createElement('div');
+                    div.classList.add('progress');
+                    let childDiv = document.createElement('div');
+                    childDiv.setAttribute('role', 'progressbar');
+                    childDiv.classList.add('progress-bar', 'bg-success');
+                    div.appendChild(childDiv);
+                    progress_bar[4].parentNode.parentNode.appendChild(h4);
+                    progress_bar[4].parentNode.parentNode.appendChild(div);
+                    progress_bar[4].parentNode.classList.add('mb-4');
+                    bar_6_exist = true;
+                }
+                for (let j = 0; j < HW_NO; j++) {
+                    progress_bar = percentage_chart.querySelectorAll('[role="progressbar"]');
+                    progress_bar[j].style.width = user_percent['HW' + (j + 1).toString() + ' Total Score'].toString() + '%';
+                    progress_percentage[j].textContent = user_percent['HW' + (j + 1).toString() + ' Total Score'].toString() + '%'
+                    dropdown_button_name.textContent = '所有題目';
+                    hw_name[j].childNodes[0].textContent = 'HW' + (j + 1).toString();
+
+                }
+            }
+
+        } else {
+            progress_dropdown[i].onclick = function() {
+                if (bar_6_exist) {
+                    let bars = percentage_chart.querySelectorAll('[role="progressbar"]');
+                    let bar_6_name = percentage_chart.getElementsByTagName('h4')[5];
+                    bars[5].parentNode.parentNode.removeChild(bars[5].parentNode);
+                    bars[4].parentNode.classList.remove('mb-4');
+                    bar_6_name.parentNode.removeChild(bar_6_name)
+                    bar_6_exist = false;
+                }
+                for (let j = 0; j < 5; j++) {
+                    progress_bar[j].style.width = (user_percent['HW' + (i).toString() + '-' + (j + 1).toString()] * 5).toString() + '%';
+                    progress_percentage[j].textContent = user_percent['HW' + (i).toString() + '-' + (j + 1).toString()].toString() + '%'
+                    dropdown_button_name.textContent = 'HW ' + i.toString();
+                    hw_name[j].childNodes[0].textContent = 'Problem ' + i.toString() + '-' + (j + 1).toString();
+
+                }
+            }
+
+        }
+    }
+
+}
+initializeProgressBar();
