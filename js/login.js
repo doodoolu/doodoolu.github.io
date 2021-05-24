284cc9cf6828abf0b3a2674c3319f2d44a125ec7
Vincent <tnecniv43208@gmail.com>
2021-05-24 22:28:41 +0800

    update

--- js/login.js
+++ js/login.js
@@ -1,20 +1,28 @@
 var login_button = document.getElementsByClassName('btn btn-primary btn-user btn-block')[0]
-var email = document.getElementsByClassName('form-control form-control-user')[0]
+var student_id = document.getElementsByClassName('form-control form-control-user')[0]
 var pw = document.getElementsByClassName('form-control form-control-user')[1]
 
 var retry_pw = document.getElementsByClassName('text-center')[1]
 
 function checkPasswordValid() {
-    firebase.database().ref().on('value', snapshot => {
+    firebase.database().ref(student_id.value).on('value', snapshot => {
         var snap = snapshot.val()
-        if (snap[email.value]['Password'] == pw.value) {
-            localStorage.setItem('User', email.value);
-            window.location.href = './index.html';
-
-        } else {
+        if (snap === null) {
             retry_pw.style.color = 'red'
             retry_pw.style.fontSize = '0.9rem'
-            retry_pw.textContent = 'Wrong Username or Password, Please Try Again!'
+            retry_pw.textContent = 'You must be enrolled in PWS 109-2!'
+
+        } else {
+            if (snap['Password'] == pw.value) {
+                localStorage.setItem('User', student_id.value);
+                window.location.href = './index.html';
+
+            } else {
+                retry_pw.style.color = 'red'
+                retry_pw.style.fontSize = '0.9rem'
+                retry_pw.textContent = 'Wrong Username or Password, Please Try Again!'
+            }
+
         }
 
     })
